3f7a67d300e46ba67dd38823ae0e291e76477a2e
unknown <amanda@unomaha.edu>
2020-01-29 20:04:23 -0600

    first commet

---
+++ tripadvisors/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from datetime import datetime
+
+
+class Tripadvisor(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.IntegerField()
+    mail_id = models.CharField(max_length=100)
+    description = models.TextField(max_length=200)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.name
